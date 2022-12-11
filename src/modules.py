79cb283b74f6626caa43232ccdf0d05e8ff75192
galihmrd <galihmardiana15@gmail.com>
2022-12-11 01:21:38 +0000

    Improvements

--- src/modules.py
+++ src/modules.py
@@ -4,12 +4,23 @@ from pyrogram import Client, filters
 from config import OPEN_AI_API
 
 
-async def openAI(self)
+async def openAI(self, msg):
     openai.api_key = OPEN_AI_API
-    response = openai.Completion.create(model="text-davinci-003", prompt=self, temperature=0, max_tokens=300)
-    await message.reply(response["choices"][0]["text"])
+    try:
+       response = openai.Completion.create(model="text-davinci-003", prompt=self, temperature=0.13, max_tokens=700)
+    except:
+       pass
+    await msg.edit(response["choices"][0]["text"])
 
-@Client.on_message(filters.command("tanya"))
+@Client.on_message(filters.command("tanya") & filters.group)
 async def tanyabot(client, message):
     prompt = " ".join(message.command[1:])
-    await openAI(prompt)
+    msg = await message.reply("Processing...")
+    await openAI(prompt, msg)
+
+
+@Client.on_message(filters.text & filters.private)
+async def tanyabot_priv(client, message):
+    prompt = message.text
+    msg = await message.reply("Processing...")
+    await openAI(prompt, msg)
